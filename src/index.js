3eefe69be3a9c997d5423d056e0e1aba628e60d8
Vladislav Akashev <vlad.akashev.w@gmail.com>
2019-02-08 11:44:55 +0300

    add eslint remarks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync'; // импорт библиотеки для чтения пользовательского ввода
 
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}`);
+  console.log(`Hello, ${username}!`);
 };
+export default greeting;
